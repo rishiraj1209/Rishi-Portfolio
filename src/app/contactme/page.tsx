b00f4ea161937b43acbd0a281ef6dab10618e227
Rishi Raj <rishiraj825268@gmail.com>
2026-04-08 20:57:46 +0530

    added the experience section

--- src/app/contactme/page.tsx
+++ src/app/contactme/page.tsx
@@ -43,7 +43,7 @@ const page = () => {
   return (
     <Container className='min-h-screen pt-30 px-10'>
         <h1 className='text-center text-4xl font-semibold text-primary dark:text-neutral-100 mb-4 text-shadow-md dark:text-shadow-neutral-700'>Contact Me</h1>
-        <div className='flex justify-center items-center border border-neutral-200 dark:border-neutral-600 rounded-lg px-8 py-10 shadow-sm shadow-black dark:shadow-neutral-500 bg-neutral-200 dark:bg-neutral-700'>
+        <div className='flex max-w-2xl mx-auto justify-center items-center border border-neutral-200 dark:border-neutral-600 rounded-lg px-8 py-10 shadow-sm shadow-black dark:shadow-neutral-500 bg-neutral-50 dark:bg-neutral-800'>
             <form onSubmit={handleSubmit} className='flex flex-col gap-4 w-full'>
                 <input className='outline-0 border border-slate-500 dark:border-zinc-500 rounded-lg px-4 py-2 shadow-aceternity dark:shadow-aceternity-dark'  type="text" placeholder='Enter your name' value={form.name} onChange={(e)=>setForm({...form, name:e.target.value})}/>
                 <input className='outline-0 border border-slate-500 dark:border-zinc-500 rounded-lg px-4 py-2 shadow-aceternity dark:shadow-aceternity-dark' type="email" placeholder='Enter your email' value={form.email} onChange={(e)=>setForm({...form, email:e.target.value})}/>
---
+++ src/app/experience/page.tsx
@@ -0,0 +1,76 @@
+'use client'
+import Container from '@/src/components/Container'
+import Skill from '@/src/components/Skill'
+import experiences from '@/src/data/experiences'
+import React from 'react'
+import { motion } from 'motion/react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen flex items-start justify-start'>
+        <Container className='py-20 min-h-screen px-4'>
+            <div className='mb-10'>
+              <h1 className='text-3xl font-semibold text-primary dark:text-neutral-100 mb-2'>Experience</h1>
+              <p className='font-medium text-secondary dark:text-neutral-400 text-sm md:text-base'>My work experiences across different companies and roles.</p>
+            </div>
+
+            <motion.div
+             initial={{
+                y:20,
+                opacity:0,
+                filter:"blur(10px)"
+             }}
+             animate={{
+                y:0,
+                opacity:1,
+                filter:"blur(0px)"
+             }}
+             transition={{
+                duration:0.3,
+                ease:'easeInOut'
+             }}
+            >
+                {experiences.map((exp,idx)=>(
+                    <div key={idx} className='my-4'>
+                        <div  className='flex justify-between items-center border-b border-neutral-200 dark:border-neutral-700'>
+                            <div className='flex flex-col items-start'>
+                                <span className='text-primary dark:text-neutral-100 font-semibold text-lg'>{exp.company} {exp.isCurrent && <span className='ml-2 bg-green-100 border border-green-500 px-2 py-1 text-[12px] font-light rounded-md text-neutral-700 dark:text-neutral-100 dark:bg-green-950 dark:border-green-700'>Working</span>}</span>
+                                <span className='text-secondary dark:text-neutral-400'>{exp.position}</span>
+                            </div>
+                            <div className='flex flex-col items-end'>
+                                <span className='text-secondary dark:text-neutral-400'>{exp.startDate} - {exp.endDate}</span>
+                                <span className='text-secondary dark:text-neutral-400'>{exp.location}</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='my-4'>
+                                <p className='text-lg font-medium'>Technologies and Tools</p>
+                                <div className='flex flex-wrap gap-2 items-center'>
+                                    {exp.technologies && exp.technologies.map((tech,idx)=>(
+                                        <Skill key={idx} name={tech.name} href={tech.href}>
+                                            {tech.icon}
+                                        </Skill>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div>
+                                <p className='text-lg font-medium'>What I've done</p>
+                                <div>
+                                    {exp.discription.map((des,idx)=>(
+                                        <p key={idx} className='text-sm text-neutral-700 dark:text-neutral-300'>-{des}</p>
+                                    ))}
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                ))}
+            </motion.div>
+        </Container>
+    </div>
+  )
+}
+
+export default page
